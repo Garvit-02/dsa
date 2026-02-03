277e18faa25b9330f1ba4f48998e58b450498031
Garvit Gupta <garvitgupta563@gmail.com>
2026-02-04 01:52:41 +0530

    Add sliding window solutions

---
+++ Sliding Window/Problem_1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int n = arr.length;
+        if(n<k) return -1;
+        
+        int windowSum = 0;
+        for(int i=0; i<k; i++){
+            windowSum += arr[i];
+        }
+        
+        int maxSum = windowSum;
+        for(int i =k; i<n; i++){
+            windowSum += arr[i] - arr[i-k];
+            maxSum = Math.max(windowSum,maxSum);
+        }
+        return maxSum;
+    }
+}
